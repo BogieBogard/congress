dfd68f37e6a93288103ceb7ee88567cd4b43d9be
BogieBogard <mbogard@utexas.edu>
2022-09-28 16:59:19 -0500

    Run automation

--- rawData/houseFiles/data2.js
+++ rawData/houseFiles/data2.js
@@ -18,7 +18,7 @@ const house2Votes =
   "source_url": "https://clerk.house.gov/evs/2022/roll453.xml",
   "subject": "Break the Cycle of Violence Act",
   "type": "On Passage of the Bill",
-  "updated_at": "2022-09-28T10:45:20-04:00",
+  "updated_at": "2022-09-28T16:52:19-04:00",
   "vote_id": "h453-117.2022",
   "votes": {
     "Nay": [
--- rawData/senateFiles/data8.js
+++ rawData/senateFiles/data8.js
@@ -23,7 +23,7 @@ const senate8Votes =
     "number": "1"
   },
   "type": "On the Amendment",
-  "updated_at": "2022-09-28T10:49:18-04:00",
+  "updated_at": "2022-09-28T16:56:13-04:00",
   "vote_id": "s342-117.2022",
   "votes": {
     "Nay": [],
