69d967394092e352a48a69f4649a9b496ea4f902
BogieBogard <mbogard@utexas.edu>
2022-10-01 00:03:18 -0500

    Run automation

--- rawData/houseFiles/data6.js
+++ rawData/houseFiles/data6.js
@@ -18,7 +18,7 @@ const house6Votes =
   "source_url": "https://clerk.house.gov/evs/2022/roll472.xml",
   "subject": "Millennium Challenge Corporation Eligibility Expansion Act",
   "type": "On Motion to Suspend the Rules and Pass",
-  "updated_at": "2022-09-30T17:49:10-04:00",
+  "updated_at": "2022-09-30T23:56:17-04:00",
   "vote_id": "h472-117.2022",
   "votes": {
     "Nay": [
