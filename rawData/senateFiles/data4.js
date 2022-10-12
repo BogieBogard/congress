8ad891d2cb81d3475f394d84f34b72929370d6ea
BogieBogard <mbogard@utexas.edu>
2022-10-12 04:08:56 -0500

    Run automation

--- rawData/senateFiles/data4.js
+++ rawData/senateFiles/data4.js
@@ -20,7 +20,7 @@ const senate4Votes =
   "source_url": "https://www.senate.gov/legislative/LIS/roll_call_votes/vote1172/vote_117_2_00349.xml",
   "subject": "On Cloture on the Motion to Proceed H.R. 6833",
   "type": "On the Cloture Motion",
-  "updated_at": "2022-10-11T21:58:55-04:00",
+  "updated_at": "2022-10-12T04:06:00-04:00",
   "vote_id": "s349-117.2022",
   "votes": {
     "Nay": [
