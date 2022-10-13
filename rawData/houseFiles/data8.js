991f24de5478cebe1be0c748441e06535bf5297a
BogieBogard <mbogard@utexas.edu>
2022-10-12 22:30:37 -0500

    Run automation

--- rawData/houseFiles/data8.js
+++ rawData/houseFiles/data8.js
@@ -18,7 +18,7 @@ const house8Votes =
   "source_url": "https://clerk.house.gov/evs/2022/roll470.xml",
   "subject": "Supporting Families of the Fallen Act",
   "type": "On Motion to Suspend the Rules and Pass",
-  "updated_at": "2022-10-12T16:16:13-04:00",
+  "updated_at": "2022-10-12T22:23:23-04:00",
   "vote_id": "h470-117.2022",
   "votes": {
     "Nay": [],
