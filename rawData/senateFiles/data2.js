cfedd02920e19a6781e663f2dad6261762c05de8
BogieBogard <mbogard@utexas.edu>
2022-10-15 05:35:00 -0500

    Run automation

--- rawData/senateFiles/data2.js
+++ rawData/senateFiles/data2.js
@@ -20,7 +20,7 @@ const senate2Votes =
   "source_url": "https://www.senate.gov/legislative/LIS/roll_call_votes/vote1172/vote_117_2_00351.xml",
   "subject": "H.R. 6833, As Amended",
   "type": "On Passage of the Bill",
-  "updated_at": "2022-10-14T23:24:53-04:00",
+  "updated_at": "2022-10-15T05:31:59-04:00",
   "vote_id": "s351-117.2022",
   "votes": {
     "Nay": [
