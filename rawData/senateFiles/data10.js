8a5f084dfee859c3bdeb0f7fa7793fd433da46de
BogieBogard <mbogard@utexas.edu>
2022-10-31 15:32:18 -0500

    Run automation

--- rawData/senateFiles/data10.js
+++ rawData/senateFiles/data10.js
@@ -17,7 +17,7 @@ const senate10Votes =
     "title": "Amendment to the Montreal Protocol on Substances that Deplete the Ozone Layer (the \"Montreal Protocol\"), adopted at Kigali on October 15, 2016, by the Twenty-Eighth Meeting of the Parties to the Montreal Protocol (the \"Kigali Amendment\")"
   },
   "type": "On the Resolution of Ratification",
-  "updated_at": "2022-10-31T09:21:07-04:00",
+  "updated_at": "2022-10-31T15:28:13-04:00",
   "vote_id": "s343-117.2022",
   "votes": {
     "Nay": [
