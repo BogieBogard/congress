12aa8d621e6102270e5b44e084a40e74514705a2
BogieBogard <mbogard@utexas.edu>
2022-11-01 03:46:42 -0500

    Run automation

--- rawData/houseFiles/data9.js
+++ rawData/houseFiles/data9.js
@@ -18,7 +18,7 @@ const house9Votes =
   "source_url": "https://clerk.house.gov/evs/2022/roll469.xml",
   "subject": "Food Security for All Veterans Act",
   "type": "On Motion to Suspend the Rules and Pass, as Amended",
-  "updated_at": "2022-10-31T21:32:29-04:00",
+  "updated_at": "2022-11-01T03:39:45-04:00",
   "vote_id": "h469-117.2022",
   "votes": {
     "Nay": [
--- rawData/senateFiles/data9.js
+++ rawData/senateFiles/data9.js
@@ -18,7 +18,7 @@ const senate9Votes =
   "source_url": "https://www.senate.gov/legislative/LIS/roll_call_votes/vote1172/vote_117_2_00344.xml",
   "subject": "On the Cloture Motion PN1566",
   "type": "On the Cloture Motion",
-  "updated_at": "2022-10-31T21:36:31-04:00",
+  "updated_at": "2022-11-01T03:43:47-04:00",
   "vote_id": "s344-117.2022",
   "votes": {
     "Nay": [
