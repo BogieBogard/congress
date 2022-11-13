02bc3629cda530e42d5d1c1a9cffc20b6ff48aec
BogieBogard <mbogard@utexas.edu>
2022-11-13 17:46:23 -0600

    Run automation

--- rawData/houseFiles/data7.js
+++ rawData/houseFiles/data7.js
@@ -18,7 +18,7 @@ const house7Votes =
   "source_url": "https://clerk.house.gov/evs/2022/roll471.xml",
   "subject": "Global Food Security Reauthorization Act",
   "type": "On Motion to Suspend the Rules and Pass, as Amended",
-  "updated_at": "2022-11-13T11:32:14-05:00",
+  "updated_at": "2022-11-13T17:39:26-05:00",
   "vote_id": "h471-117.2022",
   "votes": {
     "Nay": [
--- rawData/senateFiles/data4.js
+++ rawData/senateFiles/data4.js
@@ -20,7 +20,7 @@ const senate4Votes =
   "source_url": "https://www.senate.gov/legislative/LIS/roll_call_votes/vote1172/vote_117_2_00349.xml",
   "subject": "On Cloture on the Motion to Proceed H.R. 6833",
   "type": "On the Cloture Motion",
-  "updated_at": "2022-11-13T11:36:15-05:00",
+  "updated_at": "2022-11-13T17:43:27-05:00",
   "vote_id": "s349-117.2022",
   "votes": {
     "Nay": [
