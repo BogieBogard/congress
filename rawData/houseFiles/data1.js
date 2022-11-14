bad6f00ebe087becebc846f87080f1a2d875c497
BogieBogard <mbogard@utexas.edu>
2022-11-13 23:53:32 -0600

    Run automation

--- rawData/houseFiles/data1.js
+++ rawData/houseFiles/data1.js
@@ -18,7 +18,7 @@ const house1Votes =
   "source_url": "https://clerk.house.gov/evs/2022/roll477.xml",
   "subject": "Fairness for 9/11 Families Act",
   "type": "On Passage of the Bill",
-  "updated_at": "2022-11-13T17:39:23-05:00",
+  "updated_at": "2022-11-13T23:46:30-05:00",
   "vote_id": "h477-117.2022",
   "votes": {
     "Nay": [
