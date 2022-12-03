2544124648244642ca90468d473e888cd90784b2
BogieBogard <mbogard@utexas.edu>
2022-12-02 19:15:28 -0600

    Run automation

--- rawData/houseFiles/data4.js
+++ rawData/houseFiles/data4.js
@@ -18,7 +18,7 @@ const house4Votes =
   "source_url": "https://clerk.house.gov/evs/2022/roll497.xml",
   "subject": "DOULA for VA Act of 2022",
   "type": "On Motion to Suspend the Rules and Pass, as Amended",
-  "updated_at": "2022-12-02T13:00:30-05:00",
+  "updated_at": "2022-12-02T19:08:03-05:00",
   "vote_id": "h497-117.2022",
   "votes": {
     "Nay": [
--- rawData/senateFiles/data6.js
+++ rawData/senateFiles/data6.js
@@ -18,7 +18,7 @@ const senate6Votes =
   "source_url": "https://www.senate.gov/legislative/LIS/roll_call_votes/vote1172/vote_117_2_00367.xml",
   "subject": "Confirmation: Robert Phillip Storch, of the District of Columbia, to be Inspector General, Department of Defense",
   "type": "On the Nomination",
-  "updated_at": "2022-12-02T13:04:45-05:00",
+  "updated_at": "2022-12-02T19:12:18-05:00",
   "vote_id": "s367-117.2022",
   "votes": {
     "Nay": [
