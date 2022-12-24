84aae4c2169005b70532bebdaab82a91f3b555d0
BogieBogard <mbogard@utexas.edu>
2022-12-24 12:28:11 -0600

    Run automation

--- rawData/houseFiles/data10.js
+++ rawData/houseFiles/data10.js
@@ -18,7 +18,7 @@ const house10Votes =
   "source_url": "https://clerk.house.gov/evs/2022/roll540.xml",
   "subject": "National Heritage Area Act",
   "type": "On Passage of the Bill",
-  "updated_at": "2022-12-24T06:11:46-05:00",
+  "updated_at": "2022-12-24T12:20:04-05:00",
   "vote_id": "h540-117.2022",
   "votes": {
     "Nay": [
--- rawData/houseFiles/data8.js
+++ rawData/houseFiles/data8.js
@@ -18,7 +18,7 @@ const house8Votes =
   "source_url": "https://clerk.house.gov/evs/2022/roll542.xml",
   "subject": "Hualapai Tribe Water Rights Settlement Act",
   "type": "On Motion to Suspend the Rules and Pass",
-  "updated_at": "2022-12-24T06:11:45-05:00",
+  "updated_at": "2022-12-24T12:20:03-05:00",
   "vote_id": "h542-117.2022",
   "votes": {
     "Nay": [
