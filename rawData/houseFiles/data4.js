677591612e2c4ec1cccd35b169f84b5301787ac0
BogieBogard <mbogard@utexas.edu>
2022-12-25 19:09:48 -0600

    Run automation

--- rawData/houseFiles/data4.js
+++ rawData/houseFiles/data4.js
@@ -18,7 +18,7 @@ const house4Votes =
   "source_url": "https://clerk.house.gov/evs/2022/roll546.xml",
   "subject": "Student Veteran Emergency Relief Act",
   "type": "On Motion to Suspend the Rules and Concur in the Senate Amendment",
-  "updated_at": "2022-12-25T12:53:15-05:00",
+  "updated_at": "2022-12-25T19:01:37-05:00",
   "vote_id": "h546-117.2022",
   "votes": {
     "Nay": [
--- rawData/senateFiles/data4.js
+++ rawData/senateFiles/data4.js
@@ -25,7 +25,7 @@ const senate4Votes =
   "source_url": "https://www.senate.gov/legislative/LIS/roll_call_votes/vote1172/vote_117_2_00418.xml",
   "subject": "Klobuchar Amendment No. 6597",
   "type": "On the Amendment",
-  "updated_at": "2022-12-25T12:57:53-05:00",
+  "updated_at": "2022-12-25T19:06:15-05:00",
   "vote_id": "s418-117.2022",
   "votes": {
     "Nay": [
