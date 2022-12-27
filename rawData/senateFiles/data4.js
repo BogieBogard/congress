5d91c36ab7ab24b41e9cfe115d2fba51a8c27828
BogieBogard <mbogard@utexas.edu>
2022-12-27 01:51:24 -0600

    Run automation

--- rawData/senateFiles/data4.js
+++ rawData/senateFiles/data4.js
@@ -25,7 +25,7 @@ const senate4Votes =
   "source_url": "https://www.senate.gov/legislative/LIS/roll_call_votes/vote1172/vote_117_2_00418.xml",
   "subject": "Klobuchar Amendment No. 6597",
   "type": "On the Amendment",
-  "updated_at": "2022-12-26T19:39:34-05:00",
+  "updated_at": "2022-12-27T01:47:52-05:00",
   "vote_id": "s418-117.2022",
   "votes": {
     "Nay": [
