64ef603b59a8c2fe8b00ac553e8283949bf6b16f
BogieBogard <mbogard@utexas.edu>
2022-12-30 09:45:24 -0600

    Run automation

--- rawData/houseFiles/data8.js
+++ rawData/houseFiles/data8.js
@@ -18,7 +18,7 @@ const house8Votes =
   "source_url": "https://clerk.house.gov/evs/2022/roll542.xml",
   "subject": "Hualapai Tribe Water Rights Settlement Act",
   "type": "On Motion to Suspend the Rules and Pass",
-  "updated_at": "2022-12-30T03:28:32-05:00",
+  "updated_at": "2022-12-30T09:36:51-05:00",
   "vote_id": "h542-117.2022",
   "votes": {
     "Nay": [
