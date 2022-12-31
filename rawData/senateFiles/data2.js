a669e988706c47cb5ea3500eef1ae74b346f3dfc
BogieBogard <mbogard@utexas.edu>
2022-12-31 16:27:00 -0600

    Run automation

--- rawData/senateFiles/data2.js
+++ rawData/senateFiles/data2.js
@@ -25,7 +25,7 @@ const senate2Votes =
   "source_url": "https://www.senate.gov/legislative/LIS/roll_call_votes/vote1172/vote_117_2_00420.xml",
   "subject": "Menendez Amendment No. 6617",
   "type": "On the Amendment",
-  "updated_at": "2022-12-31T10:15:07-05:00",
+  "updated_at": "2022-12-31T16:23:27-05:00",
   "vote_id": "s420-117.2022",
   "votes": {
     "Nay": [
