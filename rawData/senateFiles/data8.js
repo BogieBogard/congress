c377d7303973eaa928fb9d969fd3c643025bd6d4
BogieBogard <mbogard@utexas.edu>
2023-01-01 10:51:59 -0600

    Run automation

--- rawData/senateFiles/data8.js
+++ rawData/senateFiles/data8.js
@@ -25,7 +25,7 @@ const senate8Votes =
   "source_url": "https://www.senate.gov/legislative/LIS/roll_call_votes/vote1172/vote_117_2_00414.xml",
   "subject": "Lankford Amendment No. 6577",
   "type": "On the Amendment",
-  "updated_at": "2023-01-01T04:40:09-05:00",
+  "updated_at": "2023-01-01T10:48:28-05:00",
   "vote_id": "s414-117.2022",
   "votes": {
     "Nay": [
