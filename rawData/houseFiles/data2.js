7f99754e89bc0044b71592c4c4f8073c29fc5c51
BogieBogard <mbogard@utexas.edu>
2023-01-02 11:25:14 -0600

    Run automation

--- rawData/houseFiles/data2.js
+++ rawData/houseFiles/data2.js
@@ -12,7 +12,7 @@ const house2Votes =
   "session": "2022",
   "source_url": "https://clerk.house.gov/evs/2022/roll548.xml",
   "type": "On the Motion to Adjourn",
-  "updated_at": "2023-01-02T05:08:44-05:00",
+  "updated_at": "2023-01-02T11:17:02-05:00",
   "vote_id": "h548-117.2022",
   "votes": {
     "Nay": [
