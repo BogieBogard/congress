0d1b467660c82f5a7a801b8efe34d95c7b40afe5
BogieBogard <mbogard@utexas.edu>
2023-02-06 09:53:46 -0600

    Run automation

--- rawData/houseFiles/data5.js
+++ rawData/houseFiles/data5.js
@@ -18,7 +18,7 @@ const house5Votes =
   "source_url": "https://clerk.house.gov/evs/2023/roll102.xml",
   "subject": "Providing for consideration of the concurrent resolution (H. Con. Res. 9) denouncing the horrors of Socialism; and providing for consideration of the resolution (H. Res. 76) removing a certain member from a certain standing committee of the House",
   "type": "On the Resolution",
-  "updated_at": "2023-02-06T03:51:51-05:00",
+  "updated_at": "2023-02-06T09:52:52-05:00",
   "vote_id": "h102-118.2023",
   "votes": {
     "Aye": [
