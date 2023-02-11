4dc617969dcf5b44889d6e413e2bc16edb859e4f
BogieBogard <mbogard@utexas.edu>
2023-02-11 16:17:24 -0600

    Run automation

--- rawData/senateFiles/data8.js
+++ rawData/senateFiles/data8.js
@@ -18,7 +18,7 @@ const senate8Votes =
   "source_url": "https://www.senate.gov/legislative/LIS/roll_call_votes/vote1181/vote_118_1_00001.xml",
   "subject": "Confirmation: Brendan Owens, of Virginia, to be an Assistant Secretary of Defense",
   "type": "On the Nomination",
-  "updated_at": "2023-02-11T10:16:15-05:00",
+  "updated_at": "2023-02-11T16:17:24-05:00",
   "vote_id": "s1-118.2023",
   "votes": {
     "Nay": [
