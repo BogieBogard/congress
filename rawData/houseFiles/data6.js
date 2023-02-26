bd11b965a05a05e47311ed35dca47f7338b7a5c5
BogieBogard <mbogard@utexas.edu>
2023-02-25 22:47:33 -0600

    Run automation

--- rawData/houseFiles/data6.js
+++ rawData/houseFiles/data6.js
@@ -22,7 +22,7 @@ const house6Votes =
   "session": "2023",
   "source_url": "https://clerk.house.gov/evs/2023/roll114.xml",
   "type": "On the Amendment",
-  "updated_at": "2023-02-25T16:45:05-05:00",
+  "updated_at": "2023-02-25T22:46:23-05:00",
   "vote_id": "h114-118.2023",
   "votes": {
     "Aye": [
--- rawData/senateFiles/data10.js
+++ rawData/senateFiles/data10.js
@@ -18,7 +18,7 @@ const senate10Votes =
   "source_url": "https://www.senate.gov/legislative/LIS/roll_call_votes/vote1181/vote_118_1_00015.xml",
   "subject": "Confirmation: Matthew L. Garcia, of New Mexico, to be U.S. District Judge for the District of New Mexico",
   "type": "On the Nomination",
-  "updated_at": "2023-02-25T16:46:08-05:00",
+  "updated_at": "2023-02-25T22:47:26-05:00",
   "vote_id": "s15-118.2023",
   "votes": {
     "Nay": [
