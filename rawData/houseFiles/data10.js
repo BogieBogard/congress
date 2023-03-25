9c00663ef3f4566fd003ca67396b2a40d084e609
BogieBogard <mbogard@utexas.edu>
2023-03-25 15:49:13 -0500

    Run automation

--- rawData/houseFiles/data10.js
+++ rawData/houseFiles/data10.js
@@ -22,7 +22,7 @@ const house10Votes =
   "session": "2023",
   "source_url": "https://clerk.house.gov/evs/2023/roll153.xml",
   "type": "On the Amendment",
-  "updated_at": "2023-03-25T09:45:18-04:00",
+  "updated_at": "2023-03-25T15:47:21-04:00",
   "vote_id": "h153-118.2023",
   "votes": {
     "Aye": [
--- rawData/houseFiles/data2.js
+++ rawData/houseFiles/data2.js
@@ -18,7 +18,7 @@ const house2Votes =
   "source_url": "https://clerk.house.gov/evs/2023/roll161.xml",
   "subject": "Parents Bill of Rights Act",
   "type": "On Passage of the Bill",
-  "updated_at": "2023-03-25T09:45:14-04:00",
+  "updated_at": "2023-03-25T15:47:17-04:00",
   "vote_id": "h161-118.2023",
   "votes": {
     "Aye": [
