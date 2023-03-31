cb0843eaf580f956912e8878f38b72406698d396
BogieBogard <mbogard@utexas.edu>
2023-03-31 10:42:20 -0500

    Run automation

--- rawData/houseFiles/data4.js
+++ rawData/houseFiles/data4.js
@@ -22,7 +22,7 @@ const house4Votes =
   "session": "2023",
   "source_url": "https://clerk.house.gov/evs/2023/roll179.xml",
   "type": "On the Amendment",
-  "updated_at": "2023-03-31T04:37:44-04:00",
+  "updated_at": "2023-03-31T10:40:03-04:00",
   "vote_id": "h179-118.2023",
   "votes": {
     "Aye": [
--- rawData/senateFiles/data6.js
+++ rawData/senateFiles/data6.js
@@ -20,7 +20,7 @@ const senate6Votes =
   "source_url": "https://www.senate.gov/legislative/LIS/roll_call_votes/vote1181/vote_118_1_00077.xml",
   "subject": "S. 316",
   "type": "On Passage of the Bill",
-  "updated_at": "2023-03-31T04:39:17-04:00",
+  "updated_at": "2023-03-31T10:41:39-04:00",
   "vote_id": "s77-118.2023",
   "votes": {
     "Nay": [
