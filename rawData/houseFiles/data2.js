267552cded9f13688089ec02bdf94624cc1839d6
BogieBogard <mbogard@utexas.edu>
2023-04-03 05:07:46 -0500

    Run automation

--- rawData/houseFiles/data2.js
+++ rawData/houseFiles/data2.js
@@ -18,7 +18,7 @@ const house2Votes =
   "source_url": "https://clerk.house.gov/evs/2023/roll181.xml",
   "subject": "Lower Energy Costs Act",
   "type": "On the Motion to Recommit",
-  "updated_at": "2023-04-02T23:03:15-04:00",
+  "updated_at": "2023-04-03T05:05:33-04:00",
   "vote_id": "h181-118.2023",
   "votes": {
     "Nay": [
