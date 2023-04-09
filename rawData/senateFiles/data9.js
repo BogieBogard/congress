f97f2455b464f883f60df3541e60a5abcdf76782
BogieBogard <mbogard@utexas.edu>
2023-04-08 22:34:28 -0500

    Run automation

--- rawData/senateFiles/data9.js
+++ rawData/senateFiles/data9.js
@@ -20,7 +20,7 @@ const senate9Votes =
   "source_url": "https://www.senate.gov/legislative/LIS/roll_call_votes/vote1181/vote_118_1_00074.xml",
   "subject": "Sullivan Amdt. No. 33",
   "type": "On the Amendment",
-  "updated_at": "2023-04-08T20:35:16-04:00",
+  "updated_at": "2023-04-08T22:33:51-04:00",
   "vote_id": "s74-118.2023",
   "votes": {
     "Nay": [
