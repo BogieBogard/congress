118e359b8046e83a18714b3cb172f18bb09563d8
BogieBogard <mbogard@utexas.edu>
2023-04-11 20:53:29 -0500

    Run automation

--- rawData/houseFiles/data1.js
+++ rawData/houseFiles/data1.js
@@ -18,7 +18,7 @@ const house1Votes =
   "source_url": "https://clerk.house.gov/evs/2023/roll182.xml",
   "subject": "Lower Energy Costs Act",
   "type": "On Passage of the Bill",
-  "updated_at": "2023-04-11T14:47:45-04:00",
+  "updated_at": "2023-04-11T20:51:14-04:00",
   "vote_id": "h182-118.2023",
   "votes": {
     "Nay": [
