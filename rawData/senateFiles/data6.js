7310301e3dc0afe12a93f335743726175c7a0467
BogieBogard <mbogard@utexas.edu>
2023-04-15 05:37:06 -0500

    Run automation

--- rawData/senateFiles/data6.js
+++ rawData/senateFiles/data6.js
@@ -20,7 +20,7 @@ const senate6Votes =
   "source_url": "https://www.senate.gov/legislative/LIS/roll_call_votes/vote1181/vote_118_1_00077.xml",
   "subject": "S. 316",
   "type": "On Passage of the Bill",
-  "updated_at": "2023-04-14T23:34:09-04:00",
+  "updated_at": "2023-04-15T05:36:27-04:00",
   "vote_id": "s77-118.2023",
   "votes": {
     "Nay": [
