2250bbf4ceae7dbd1b1a2cf96ab26076dcfb81ba
BogieBogard <mbogard@utexas.edu>
2023-04-16 05:46:21 -0500

    Run automation

--- rawData/houseFiles/data4.js
+++ rawData/houseFiles/data4.js
@@ -22,7 +22,7 @@ const house4Votes =
   "session": "2023",
   "source_url": "https://clerk.house.gov/evs/2023/roll179.xml",
   "type": "On the Amendment",
-  "updated_at": "2023-04-15T23:41:50-04:00",
+  "updated_at": "2023-04-16T05:44:09-04:00",
   "vote_id": "h179-118.2023",
   "votes": {
     "Aye": [
--- rawData/houseFiles/data9.js
+++ rawData/houseFiles/data9.js
@@ -22,7 +22,7 @@ const house9Votes =
   "session": "2023",
   "source_url": "https://clerk.house.gov/evs/2023/roll174.xml",
   "type": "On the Amendment",
-  "updated_at": "2023-04-15T23:41:53-04:00",
+  "updated_at": "2023-04-16T05:44:12-04:00",
   "vote_id": "h174-118.2023",
   "votes": {
     "Aye": [
