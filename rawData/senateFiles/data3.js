78538cac6147ce9b85a626262e68979767a1d707
BogieBogard <mbogard@utexas.edu>
2023-04-30 20:23:06 -0500

    Run automation

--- rawData/senateFiles/data3.js
+++ rawData/senateFiles/data3.js
@@ -20,7 +20,7 @@ const senate3Votes =
   "source_url": "https://www.senate.gov/legislative/LIS/roll_call_votes/vote1181/vote_118_1_00098.xml",
   "subject": "S. J. Res. 11",
   "type": "On the Joint Resolution",
-  "updated_at": "2023-04-30T14:19:35-04:00",
+  "updated_at": "2023-04-30T20:22:15-04:00",
   "vote_id": "s98-118.2023",
   "votes": {
     "Nay": [
