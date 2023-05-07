89276e71594b8089a32a7b15df8eeff7e3942350
BogieBogard <mbogard@utexas.edu>
2023-05-07 13:09:36 -0500

    Run automation

--- rawData/houseFiles/data4.js
+++ rawData/houseFiles/data4.js
@@ -18,7 +18,7 @@ const house4Votes =
   "source_url": "https://clerk.house.gov/evs/2023/roll199.xml",
   "subject": "Limit, Save, Grow Act",
   "type": "On Passage of the Bill",
-  "updated_at": "2023-05-07T06:40:18-04:00",
+  "updated_at": "2023-05-07T13:06:32-04:00",
   "vote_id": "h199-118.2023",
   "votes": {
     "Nay": [
--- rawData/senateFiles/data10.js
+++ rawData/senateFiles/data10.js
@@ -18,7 +18,7 @@ const senate10Votes =
   "source_url": "https://www.senate.gov/legislative/LIS/roll_call_votes/vote1181/vote_118_1_00106.xml",
   "subject": "On the Cloture Motion PN180",
   "type": "On the Cloture Motion",
-  "updated_at": "2023-05-07T06:42:18-04:00",
+  "updated_at": "2023-05-07T13:08:37-04:00",
   "vote_id": "s106-118.2023",
   "votes": {
     "Nay": [
