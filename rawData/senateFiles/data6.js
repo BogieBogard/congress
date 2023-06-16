9ac91afce0f12077bc09d7024b9bd527565a2b99
BogieBogard <mbogard@utexas.edu>
2023-06-16 01:29:27 -0500

    Run automation

--- rawData/senateFiles/data6.js
+++ rawData/senateFiles/data6.js
@@ -18,7 +18,7 @@ const senate6Votes =
   "source_url": "https://www.senate.gov/legislative/LIS/roll_call_votes/vote1181/vote_118_1_00160.xml",
   "subject": "On the Cloture Motion PN86",
   "type": "On the Cloture Motion",
-  "updated_at": "2023-06-15T14:48:20-04:00",
+  "updated_at": "2023-06-16T01:27:54-04:00",
   "vote_id": "s160-118.2023",
   "votes": {
     "Nay": [
