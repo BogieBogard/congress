0df4daeb4d70ea60adf10b3dd2454df33222b942
BogieBogard <mbogard@utexas.edu>
2023-09-05 16:39:47 -0500

    Run automation

--- rawData/houseFiles/data8.js
+++ rawData/houseFiles/data8.js
@@ -22,7 +22,7 @@ const house8Votes =
   "session": "2023",
   "source_url": "https://clerk.house.gov/evs/2023/roll375.xml",
   "type": "On the Amendment",
-  "updated_at": "2023-09-02T17:12:16-04:00",
+  "updated_at": "2023-09-05T16:34:51-04:00",
   "vote_id": "h375-118.2023",
   "votes": {
     "Aye": [
